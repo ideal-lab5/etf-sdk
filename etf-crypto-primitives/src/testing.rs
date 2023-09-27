2a0a2f834d2e4f1f31d37a1232f7198188fe013d
driemworks <tonyrriemer@gmail.com>
2023-09-26 19:58:40 -0500

    add test functions

--- etf-crypto-primitives/src/testing.rs
+++ etf-crypto-primitives/src/testing.rs
@@ -8,30 +8,53 @@ use crate::utils::*;
 /// generate pseudorandom ibe params to seed the IBE
 /// returns (P, P_{pub}, s) where P_{pub} = sP
 pub fn test_ibe_params() -> (Vec<u8>, Vec<u8>, Vec<u8>) {
-    let ibe_pp: G2 = G2::generator();
-    let s = Fr::rand(&mut test_rng());
-    let p_pub: G2 = ibe_pp.mul(s).into();
+    let g = test_ibe_params_typed();
     (
-        convert_to_bytes::<G2, 96>(ibe_pp).to_vec(),
-        convert_to_bytes::<G2, 96>(p_pub).to_vec(),
-        convert_to_bytes::<Fr, 32>(s).to_vec()
+        convert_to_bytes::<G2, 96>(g.0).to_vec(),
+        convert_to_bytes::<G2, 96>(g.1).to_vec(),
+        convert_to_bytes::<Fr, 32>(g.2).to_vec()
     )
     
 }
 
+/// generate pseudorandom ibe params to seed the IBE
+/// returns (P, P_{pub}, s) where P_{pub} = sP
+pub fn test_ibe_params_typed() -> (G2, G2, Fr) {
+    let ibe_pp: G2 = G2::generator();
+    let s = Fr::rand(&mut test_rng());
+    let p_pub: G2 = ibe_pp.mul(s).into();
+    (ibe_pp, p_pub, s)
+}
+
 /// perform the IBE EXTRACT phase of the BF IBE
 /// this can be used to simulate the ETF network
+///
+/// returns a list of (sk, pk)
 ///
 /// * `x`: The secret key as bytes
 /// * `ids`: the slot ids
 ///
 pub fn ibe_extract(x: Vec<u8>, ids: Vec<Vec<u8>>) -> Vec<(Vec<u8>, Vec<u8>)> {
     let s = Fr::deserialize_compressed(&x[..]).unwrap();
+    ibe_exract_typed(s, ids).iter().map(|x| {
+        let pk_bytes = convert_to_bytes::<G1, 48>(x.1);
+        let sk_bytes = convert_to_bytes::<G1, 48>(x.0);
+        (sk_bytes.to_vec(), pk_bytes.to_vec())
+    }).collect::<Vec<_>>()
+}
+
+/// perform the IBE EXTRACT phase of the BF IBE
+/// this can be used to simulate the ETF network
+///
+/// returns a list of (sk, pk)
+///
+/// * `s`: The secret key
+/// * `ids`: the slot ids
+///
+pub fn ibe_exract_typed(s: Fr, ids: Vec<Vec<u8>>) -> Vec<(G1, G1)> {
     ids.iter().map(|id| {
         let pk = hash_to_g1(id);
-        let sk = pk.mul(s);
-        let pk_bytes = convert_to_bytes::<G1, 48>(pk);
-        let sk_bytes = convert_to_bytes::<G1, 48>(sk.into());
-        (sk_bytes.to_vec(), pk_bytes.to_vec())
+        let sk = pk.mul(s).into();
+        (sk, pk)
     }).collect::<Vec<_>>()
 }
