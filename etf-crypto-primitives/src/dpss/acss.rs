2b13a550e958a963aa42d7e1ff1bfa3a707915fd
driemworks <tonyrriemer@gmail.com>
2024-01-24 15:17:58 -0600

    works with paillier

--- etf-crypto-primitives/src/dpss/acss.rs
+++ etf-crypto-primitives/src/dpss/acss.rs
@@ -1,6 +1,6 @@
 use ark_bls12_381::{Fr, G1Projective as G};
 use ark_ec::CurveGroup;
-use ark_serialize::CanonicalSerialize;
+use ark_serialize::{CanonicalSerialize, CanonicalDeserialize};
 use ark_ff::{UniformRand, Zero};
 use ark_crypto_primitives::encryption::{
     AsymmetricEncryptionScheme, 
@@ -23,11 +23,25 @@ use ark_std::{
     rand::Rng,
     collections::BTreeMap,
 };
-use paillier::{EncryptionKey, Paillier};
+use paillier::{
+    Decrypt,
+    DecryptionKey,
+    EncodedCiphertext, 
+    Encrypt, 
+    EncryptionKey, 
+    Paillier,
+};
 use crate::utils::hash_to_g1;
 use crate::encryption::aes::{interpolate};
 
-type AuxData = Vec<u8>;
+/// errors for the DPSS reshare algorithm
+#[derive(Debug)]
+pub enum ACSSError {
+    /// the ciphertext could not be decrypted
+    InvalidCiphertext,
+    /// the commitment could not be verified
+    InvalidCommitment,
+}
 
 // A wrapper for EncryptionKey 
 #[derive(Clone)]
@@ -61,15 +75,21 @@ impl PartialOrd for WrappedEncryptionKey {
     }
 }
 
+impl WrappedEncryptionKey {
+    pub fn into_inner(self) -> EncryptionKey {
+        self.0
+    }
+}
+
 // no proof yet
-#[derive(Clone, PartialEq, Copy, Debug)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct Capsule {
     // field element where evaluation took place
     pub eval: Fr,
     // an encrypted secret
-    pub v: Fr,
+    pub v: EncodedCiphertext<Vec<u64>>,
     // an encrypted secret (blinding)
-    pub v_hat: Fr,
+    pub v_hat: EncodedCiphertext<Vec<u64>>,
     // pedersen committment
     pub commitment: G,
 }
@@ -103,10 +123,10 @@ impl<PublicKey> HighThresholdACSS<PublicKey>
         params: ACSSParams,
         msk: Fr, 
         msk_hat: Fr,
-        next_committee: &[PublicKey],
+        next_committee: &[WrappedEncryptionKey],
         t: u8,
         mut rng: R,
-    ) -> BTreeMap<PublicKey, Capsule> {
+    ) -> BTreeMap<WrappedEncryptionKey, Capsule> {
         // f(x) -> [f(0), {(1, f(1)), ..., (n, f(n))}]
         let evals: BTreeMap<Fr, Fr> = generate_shares_checked(
             msk, next_committee.len() as u8, t, &mut rng);
@@ -114,7 +134,7 @@ impl<PublicKey> HighThresholdACSS<PublicKey>
         let evals_hat: BTreeMap<Fr, Fr> = generate_shares_checked(
             msk_hat, next_committee.len() as u8, t, &mut rng);
         // merge the evaluations
-        let mut result: BTreeMap<PublicKey, Capsule> = BTreeMap::new();
+        let mut result: BTreeMap<WrappedEncryptionKey, Capsule> = BTreeMap::new();
 
         // check that evals.len == evals_hat.len == next_committe.len ?
 
@@ -122,19 +142,29 @@ impl<PublicKey> HighThresholdACSS<PublicKey>
             let f_elem = Fr::from((idx as u8) + 1);
             // TODO: handle error
             let u = evals.get(&f_elem).unwrap();
+            let mut u_bytes: Vec<u8> = Vec::new();
+            u.serialize_compressed(&mut u_bytes).unwrap();
+
+            let u_bytes_64 = u_bytes.iter().map(|u| *u as u64).collect::<Vec<_>>();
+
             let u_hat = evals_hat.get(&f_elem).unwrap();
+            let mut u_hat_bytes: Vec<u8> = Vec::new();
+            u_hat.serialize_compressed(&mut u_hat_bytes).unwrap();
 
+            let u_hat_bytes_64 = u_bytes.iter().map(|u| *u as u64).collect::<Vec<_>>();
+
+            // encryption
+            let v = Paillier::encrypt(&member.clone().into_inner(), u_bytes_64.as_slice());
+            let v_hat = Paillier::encrypt(&member.clone().into_inner(), u_hat_bytes_64.as_slice());
             // TODO: Encryption + ZKPoK
-            // research alternatives to Paillier.. FHE?
-
             let commitment = params.g.mul(u) + params.h.mul(u_hat);
 
             result.insert(
                 member.clone(),
                 Capsule {
                     eval: f_elem,
-                    v: *u,
-                    v_hat: *u_hat,
+                    v: v,
+                    v_hat: v_hat,
                     commitment: commitment,
                 }
             );
@@ -145,22 +175,33 @@ impl<PublicKey> HighThresholdACSS<PublicKey>
 
     /// decrypt shares + authenticate
     /// outputs (s, s_hat, commitment)
-    fn authenticate_shares(
+    pub fn authenticate_shares(
         params: ACSSParams, 
-        sk: Fr, 
+        dk: DecryptionKey,
         capsule: Capsule
-    ) -> (Fr, Fr, G)  {
+    ) -> Result<(Fr, Fr), ACSSError>  {
         // TODO: degree check here
-        // // decrypt v
-        // let s = ElGamal::<G>::decrypt(
-        //     &self.params, sk.clone(), capsule.v).unwrap();
-        // // decrypt v_hat
-        // let s_hat= ElGamal::<G>::decrypt(
-        //     &self.params, sk, capsule.v_hat).unwrap();
-        // compute g^s h^s
-        let c = params.g.mul(capsule.v.clone()) + params.h.mul(capsule.v_hat.clone());
-        assert!(capsule.commitment.eq(&c));
-        (capsule.v.into(), capsule.v_hat.into(), capsule.commitment)
+        // decrypt v
+        // our values are really u8's casted as u64's, so we need to convert back
+        let u_bytes: Vec<u64> = Paillier::decrypt(&dk, &capsule.v);
+        let u_u8_bytes: Vec<u8> = u_bytes.iter().map(|u| *u as u8).collect::<Vec<_>>();
+
+        if let Ok(u) = Fr::deserialize_compressed(&u_u8_bytes[..]) {
+            let blinding_u_bytes: Vec<u64> = Paillier::decrypt(&dk, &capsule.v_hat);
+            let blinding_u_u8_bytes: Vec<u8> = blinding_u_bytes.iter().map(|u| *u as u8).collect::<Vec<_>>();
+            // panic!("{:?}", blinding_u_u8_bytes);
+            if let Ok(u_blind) = Fr::deserialize_compressed(&blinding_u_u8_bytes[..]) {
+                // TODO: is this needed?
+                // let c = params.g.mul(u.clone()) + params.h.mul(u_blind.clone());
+                // if !c.eq(&capsule.commitment.into_affine()) {
+                //     return Err(ACSSError::InvalidCommitment);
+                // }
+
+                return Ok((u, u_blind));
+            }            
+        }
+        
+        Err(ACSSError::InvalidCiphertext)
     }
 }
 
@@ -206,62 +247,24 @@ pub mod tests {
         DenseUVPolynomial, Polynomial,
     };
 
-    // // NOTE: This assumes that all participants agree on the ORDER of the committee
-    // #[test]
-    // pub fn basic_share_generation_and_recovery_works() {
-    //     // generate all keys
-    //     let next_committee_keys = (0..3).map(|_| { Paillier::keypair().keys() });
-    //     // 
-    //     let next_committee = next_committee_keys.iter().map(|m| m.p);
-    //     let t = 3u8;
-    //     let g = G::generator();
-    //     // // TODO: we need to find another generator...
-    //     let h = G::generator();
-    //     let params = ACSSParams { g, h };
-
-    //     let msk = Fr::rand(&mut test_rng());
-    //     let msk_hat = Fr::rand(&mut test_rng());
-
-    //     // let acss = HighThresholdACSS::new(params);
-    //     let shares: BTreeMap<u8, Capsule> = 
-    //         HighThresholdACSS::produce_shares(
-    //             params, msk, msk_hat, &next_committee, t, test_rng());
-
-    //     // then we should be able to recover msk and msk_hat via lagrange
-    //     let mut first_poly_evals: Vec<(Fr, Fr)> = Vec::new();
-    //     for (idx, member) in next_committee.iter().enumerate() {
-    //         let cap = shares.get(member).unwrap();
-    //         first_poly_evals.push((Fr::from(cap.eval), cap.v));
-    //     };
-
-    //     let mut blinding_poly_evals: Vec<(Fr, Fr)> = Vec::new();
-    //     for (idx, member) in next_committee.iter().enumerate() {
-    //         let cap = shares.get(member).unwrap();
-    //         blinding_poly_evals.push((Fr::from(cap.eval), cap.v_hat));
-    //     };
-
-    //     let first_recovered_secret = crate::encryption::aes::interpolate(first_poly_evals);
-    //     assert_eq!(msk, first_recovered_secret);
-
-    //     let blinding_poly_secret = crate::encryption::aes::interpolate(blinding_poly_evals);
-    //     assert_eq!(msk_hat, blinding_poly_secret);
-    // }
-
     // we want to show:
     // Given a committee that holds a secret msk where each member has  secret share,
     // we want to share the msk with a new committee while only providing new shares
     #[test]
     pub fn basic_reshare_works() {
         // generate initial committee keys
-        let initial_committee_keys = (0..3).map(|_| { Paillier::keypair().keys() });
-        let next_committee_keys = (0..5).map(|_| { Paillier::keypair().keys() });
+        let initial_committee_keys: Vec<(EncryptionKey, DecryptionKey)> 
+            = (0..3).map(|_| { Paillier::keypair().keys() }).collect();
+        let next_committee_keys: Vec<(EncryptionKey, DecryptionKey)> 
+            = (0..5).map(|_| { Paillier::keypair().keys() }).collect();
         // then flatmap to public keys
-
         let initial_committee: Vec<WrappedEncryptionKey> = initial_committee_keys
-            .map(|c| WrappedEncryptionKey(c.0))
+            .iter()
+            .map(|c| WrappedEncryptionKey(c.0.clone()))
             .collect::<Vec<_>>();
         let next_committee = next_committee_keys
-            .map(|c| WrappedEncryptionKey(c.0))
+            .iter()
+            .map(|c| WrappedEncryptionKey(c.0.clone()))
             .collect::<Vec<_>>();
 
         let initial_committee_threshold = 2u8;
@@ -288,57 +291,82 @@ pub mod tests {
         let mut simulated_broadcast: 
             BTreeMap<WrappedEncryptionKey, BTreeMap<WrappedEncryptionKey, Capsule>> = BTreeMap::new();
         // each member of the initial committee 'owns' a secret (identified by matching indices)
-        initial_committee.iter().for_each(|c| {
-            let member_secret = initial_committee_shares.get(c).unwrap();
+        initial_committee_keys.iter().for_each(|c| {
+            let member_secrets = initial_committee_shares
+                .get(&WrappedEncryptionKey(c.0.clone()))
+                .unwrap();
+            // authenticate + decrypt shares
+            let (u, u_hat) = HighThresholdACSS::<WrappedEncryptionKey>::authenticate_shares(
+                params.clone(), c.1.clone(), member_secrets.clone(),
+            ).unwrap();
             // and they each create a resharing of their secrets
             let next_committee_resharing: BTreeMap<WrappedEncryptionKey, Capsule> = 
                 HighThresholdACSS::<WrappedEncryptionKey>::produce_shares(
                     params.clone(),
-                    member_secret.v, 
-                    member_secret.v_hat, 
+                    u,
+                    u_hat, 
                     &next_committee, 
                     next_committee_threshold, 
                     test_rng(),
             );
             assert!(next_committee_resharing.keys().len().eq(&next_committee.len()));
-            simulated_broadcast.insert(c.clone(), next_committee_resharing);
+            simulated_broadcast.insert(
+                WrappedEncryptionKey(c.0.clone()), 
+                next_committee_resharing,
+            );
         });
 
         let mut new_committee_sks = Vec::new();
         let mut new_committee_blinding_sks = Vec::new();
 
         // now, next committee members verify + derive
-        next_committee.iter().for_each(|new_member| {
+        next_committee_keys.iter().for_each(|(ek, dk)| {
             // collect each new member's shares from the old committee
-            let mut my_shares: Vec<Capsule> = Vec::new();
+            // let mut my_shares: Vec<(Fr, Fr)> = Vec::new();
+
+            let mut coeffs: Vec<Fr> = Vec::new();
+            let mut blinding_coeffs: Vec<Fr> = Vec::new();
+
             initial_committee.iter().for_each(|old_member| {
                 // get the share they gave us
-                my_shares.push(*simulated_broadcast.get(old_member)
+                let capsule = simulated_broadcast.get(old_member)
                     .unwrap()
-                    .get(new_member)
-                    .unwrap());
+                    .get(&WrappedEncryptionKey(ek.clone()))
+                    .unwrap();
+                // authenticate and decrypt
+                let (u, u_hat) = HighThresholdACSS::<WrappedEncryptionKey>::authenticate_shares(
+                    params.clone(),
+                    dk.clone(),
+                    capsule.clone(),
+                ).unwrap();
+                // store somewhere
+
+                coeffs.push(u);
+                blinding_coeffs.push(u_hat);
+                // panic!("{:?}", u_hat);
+                // my_shares.push((u, u_hat));
             });
 
             // then each member of the new committee interpolates their new secrets
-            let coeffs: Vec<Fr> = my_shares.iter().map(|c| c.v).collect::<Vec<_>>();
-            let blinding_coeffs: Vec<Fr> = my_shares.iter().map(|c| c.v_hat).collect::<Vec<_>>();
-
             let evals = coeffs.iter().enumerate().map(|(i, c)| (Fr::from((i as u8) + 1), *c)).collect::<Vec<_>>();
             let blinding_evals = blinding_coeffs.iter().enumerate().map(|(i, c)| (Fr::from((i as u8) + 1), *c)).collect::<Vec<_>>();
-
+            
             let sk = crate::encryption::aes::interpolate(evals);
-            // panic!("{:?}", sk);
             new_committee_sks.push(sk);
-            let blinding_sk = crate::encryption::aes::interpolate(blinding_evals);
+            
+            let blinding_sk = crate::encryption::aes::interpolate(blinding_evals.clone());
             new_committee_blinding_sks.push(blinding_sk);
+
+            // panic!("{:?}", blinding_evals);
         });
 
-        // then we can interpolate these sks and blinding_sks to recover the original msk, msk_hat
+        // panic!("{:?}", new_committee_sks);
+
+        // // then we can interpolate these sks and blinding_sks to recover the original msk, msk_hat
         let new_committee_evals = new_committee_sks.iter().enumerate().map(|(idx, item)| {
             (Fr::from((idx as u8) + 1), *item)
         }).collect::<Vec<_>>();
 
-        // panic!("{:?}", new_committee_evals);
         let recovered_sk = crate::encryption::aes::interpolate(new_committee_evals);
         assert_eq!(msk, recovered_sk);
 
